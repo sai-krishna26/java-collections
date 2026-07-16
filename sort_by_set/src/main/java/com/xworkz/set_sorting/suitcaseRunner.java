5b3d3249531c8a3a1d6aefcc958d2c9a90761f50
sai-krishna26 <saikrishnavishwakarma@gmail.com>
2026-07-16 23:47:41 +0530

    updated task

---
+++ sort_by_set/src/main/java/com/xworkz/set_sorting/suitcaseRunner.java
@@ -0,0 +1,20 @@
+package com.xworkz.set_sorting;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class suitcaseRunner
+{
+    public static void main(String[] args)
+    {
+        SuitcaseDto suitcaseDto = new SuitcaseDto("Lg",5.6f,2000.45f,"Black");
+        SuitcaseDto suitcaseDto1 = new SuitcaseDto("maby",7.6f,3000.45f,"red");
+        SuitcaseDto suitcaseDto2 = new SuitcaseDto("bluestone",2.6f,3500.45f,"navy blue");
+        SuitcaseDto suitcaseDto3 = new SuitcaseDto("bluestone",4.5g,3500.45f,"navy blue");
+        Set<SuitcaseDto> suitcaseSet = new HashSet<>();
+
+        suitcaseSet.add()
+
+    }
+
+}
